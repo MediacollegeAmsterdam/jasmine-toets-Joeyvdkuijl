cc2234552577caf2529ee47ff0c1d6df116541b5
Joey van der Kuijl <joeyvdkuijl@hotmail.com>
2020-10-20 10:56:44 +0200

    update

--- toets/opdracht 3/student.js
+++ toets/opdracht 3/student.js
@@ -1,17 +1,23 @@
 function Student(naam, studentId) {
 
     let obj = this;
-    this.name = naam + 'Hi';
-    this.id = studentId;
+    obj.name = naam + 'Hi';
+    obj.id = studentId;
     obj.cijfers = [];
+    obj.klas = klas;
 
-    let klas = '';
-
+    let objectValues = function () {
+        console.log( obj.klas );
+    }
+    this.info = function() {
+        objectValues;
+    }
 }
 
-let student = new Student('myName', '0001');
+let student = new Student('myName', '0001', 'md2a');
 
 // Hier moet je de variabele klas een waarde geven bijvoorbeeld MD2
-console.log(
-    /* Hier moet de variabele klas getoond worden */
-)
+// console.log(
+//     /* Hier moet de variabele klas getoond worden */
+//     student.klas
+// )
